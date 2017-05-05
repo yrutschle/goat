b3d1318da814d377e4669e9bf07062f65297d10e
Richard Hitier <richard.hitier@co-libri.org>
2017-05-05 13:50:09 +0200

    New config var TOURNAMENT_FILE from goat.cfg
    'tournament_file: toperm' in goat.cfg
    is read by GoatConfig.pm to export
    $TOURNAMENT_FILE

--- GoatConfig.pm
+++ GoatConfig.pm
@@ -13,6 +13,7 @@ $INSTALL_DIR $WORK_DIR $LOG_DIR $TMP_DIR $SGF_URL
 
 $GOAT_ADDRESS $ADMIN_ADDRESS $TOURNAMENT_NAME $TOURNAMENT_CITY
 $TOURNAMENT_LICENSES
+$TOURNAMENT_FILE
 
 $TEMPLATE_DIR
 
@@ -49,6 +50,7 @@ my $cfg = LoadFile("$ENV{WORK_DIR}/goat.cfg");
 our $GOAT_ADDRESS = $cfg->{goat_address};
 our $ADMIN_ADDRESS = $cfg->{admin_address};
 our $TOURNAMENT_NAME = $cfg->{tournament_name};
+our $TOURNAMENT_FILE = $cfg->{tournament_file};
 our $TOURNAMENT_CITY = $cfg->{tournament_city};
 our $TOURNAMENT_LICENSES= $cfg->{tournament_licenses};
 our $LOCALE = $cfg->{locale};
